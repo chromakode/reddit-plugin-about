334e2633f277d0b171722c8982dc5c5bb81efdf2
Max Goodman <c@chromakode.com>
2012-02-01 17:54:28 -0800

    Restrict /about/ route to only apply to the root/front page.

--- reddit_about/__init__.py
+++ reddit_about/__init__.py
@@ -1,8 +1,11 @@
 from r2.lib.plugin import Plugin
 
+def not_in_sr(environ, results):
+    return 'subreddit' not in environ and 'sub_domain' not in environ
+
 class About(Plugin):
     def add_routes(self, mc):
-        mc('/about/:action', controller='about')
+        mc('/about/:action', controller='about', conditions={'function':not_in_sr})
 
     def load_controllers(self):
         from about import AboutController
