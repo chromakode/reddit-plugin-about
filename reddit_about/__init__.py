c5e64e25ced1fd5b93eac60ff78faff15f8243ae
Max Goodman <c@chromakode.com>
2012-02-09 14:31:13 -0800

    Add LESS.js module and template utilities.

--- reddit_about/__init__.py
+++ reddit_about/__init__.py
@@ -1,9 +1,14 @@
 from r2.lib.plugin import Plugin
+from r2.lib.js import Module
 
 def not_in_sr(environ, results):
     return 'subreddit' not in environ and 'sub_domain' not in environ
 
 class About(Plugin):
+    js = {
+        'less': Module('lib/less-1.2.1.min.js', should_compile=False)
+    }
+
     def add_routes(self, mc):
         mc('/about/:action', controller='about', conditions={'function':not_in_sr})
 
