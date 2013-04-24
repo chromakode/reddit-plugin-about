746d86ff1110d788c9988932ae7787dade06246b
Max Goodman <c@chromakode.com>
2013-04-24 04:00:09 -0700

    Improvements to scrollFixed util.
    * create stand-in element instead of cloning so DOM manipulation works.
    * add class when active for custom styling purposes.
    * only set fixed if there is enough vertical space.

--- reddit_about/public/static/js/about/about-utils.js
+++ reddit_about/public/static/js/about/about-utils.js
@@ -1,26 +1,40 @@
 function scrollFixed(el) {
     this.$el = $(el)
-    this.$clone = null
+    this.$standin = null
     this.origTop = this.$el.position().top
     this.onScroll()
-    $(window).scroll(_.bind(this.onScroll, this))
+    $(window).bind('scroll resize', _.bind(this.onScroll, this))
 }
 scrollFixed.prototype = {
     onScroll: function() {
-        if ($(window).scrollTop() > this.origTop) {
-            if (!this.$clone) {
-                this.$clone = this.$el.clone()
-                this.$clone
-                    .appendTo(this.$el.parent())
+        var enoughSpace = this.$el.height() < $(window).height()
+        if (enoughSpace && $(window).scrollTop() > this.origTop) {
+            if (!this.$standin) {
+                this.$standin = $('<div>')
+                    .css({
+                        width: this.$el.width(),
+                        height: this.$el.height()
+                    })
+                    .attr('class', this.$el.attr('class'))
+
+                this.$el
+                    .addClass('scroll-fixed')
                     .css({
                         position: 'fixed',
                         top: 0
                     })
+                this.$el.before(this.$standin)
             }
         } else {
-            if (this.$clone) {
-                this.$clone.remove()
-                this.$clone = null
+            if (this.$standin) {
+                this.$el
+                    .removeClass('scroll-fixed')
+                    .css({
+                        position: '',
+                        top: ''
+                    })
+                this.$standin.remove()
+                this.$standin = null
             }
         }
     }
